57e0b428f8778eaeace9dfbe3bca5fd7dd7de8f9
Jdrck-1918 <sjc.jaderickdelcampo@gmail.com>
2025-04-15 19:59:10 +0800

    Add files via upload

---
+++ Activity4/Staff.java
@@ -0,0 +1,19 @@
+package Activity4;
+
+public class Staff extends Person {
+    private String position;
+
+    public Staff(String name, int id, String position) {
+        super(name, id);
+        this.position = position;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Position: " + position);
+    }
+}
---
+++ Activity4/Teacher.java
@@ -0,0 +1,20 @@
+package Activity4;
+
+public class Teacher extends Person {
+    private String subject;
+
+    public Teacher(String name, int id, String subject) {
+        super(name, id);
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Subject: " + subject);
+    }
+}
